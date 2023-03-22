d0bac040518a636bd8b24dd9dd71ce3d9cea78ca
mainavict <vampmid03@gmail.com>
2023-03-22 01:10:19 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,22 +9,20 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		-_putchar('0');
+		_putchar('0');
 
 		for (mult  = 1; mult <= 9; mult++)
 		{
-			_putcha(',');
+			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
 			if (prod <= 9)
 				_putchar(' ');
 			else
-
 				_putchar((prod / 10 + '0');
-
-						_purchar((prod / 10) + '0');
-						}
-						_putchar('\n');
-						}
-						}
+				_purchar((prod / 10) + '0');
+				}
+				_putchar('\n');
+				}
+				}
