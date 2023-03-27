180d584486d36830eab46e24fc06721d55473ccc
mainavict <vampmid03@gmail.com>
2023-03-27 01:14:31 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,8 +8,8 @@
 int _atoi(char *s)
 {
 	int c = 0;
-	unsigned int ni = 0
-		int min = 1;
+	unsigned int ni = 0:
+	int min = 1;
 	int isi = 0;
 
 	while (s[c])
@@ -34,4 +34,4 @@ int _atoi(char *s)
 	}
 	ni *= min;
 	return (ni);
-}
+i}
