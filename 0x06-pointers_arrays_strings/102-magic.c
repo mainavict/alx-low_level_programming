eda3ac802e0b1c74ec1da9e8eeec4428789886b1
mainavict <vampmid03@gmail.com>
2023-03-29 23:51:58 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,6 +10,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
+	 * main - helohelo
 	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
@@ -20,5 +21,5 @@ int main(void)
 	 *(p + 5) = 98;
 	 /* ...so that this prints 98\n */
 	 printf("a[2] = %d\n", a[2]);
-	 return (0);
+return (0);
 }
