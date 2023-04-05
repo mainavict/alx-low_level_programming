1b6267011db3ab5cf8579c6ba476e1029d0cb1b2
mainavict <vampmid03@gmail.com>
2023-04-05 15:31:31 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,15 @@
 void _puts_recursion(char *s);
 {
 	if (*s)
-	{
-		_print_rev_recursion(s + 1);
+	{  
 		_putchar(*s);
+		_print_rev_recursion(s + 1);
 	}
-	_putchar(' \n ');
+	else
+	{
+		_putchar('\n');
+	}
+		
 		
 }
 
