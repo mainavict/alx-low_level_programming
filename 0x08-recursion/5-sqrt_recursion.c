ff8e79df583e6fd60c0c14c94a34771d84aeb23b
mainavict <vampmid03@gmail.com>
2023-04-05 11:33:14 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * find the squreroot of  n
+ * @n is the  number to be squred
+ *Return 0;
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+return (1 * (_sqrr_recursion n));
+	}}	 
