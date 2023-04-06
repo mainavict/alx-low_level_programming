174b313f51b7d05690c16e8ba5bda4915b772c65
mainavict <vampmid03@gmail.com>
2023-04-06 04:57:55 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	_puts_recusion("puts with recursion");
+	_puts_recursion("puts with recursion");
 	return (0);
 }
 
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,8 +9,8 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_recursion(s + 1);
 		 _putchar(*s);
+		 _puts_recursion(s + 1);
 	}
-	_putchar('\n ');
+	_putchar('\n');
 }
