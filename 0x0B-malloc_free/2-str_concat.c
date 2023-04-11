e43fe679f120be73be9856ab115d4d9a5b883642
mainavict <vampmid03@gmail.com>
2023-04-11 11:17:14 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,14 +35,12 @@ char *str_concat(char *s1, char *s2)
 			i = j = 0;
 
 			while (s1[i] != '\0')
-
 			{
 				while (s1[i] != '\0')
 
 					i++;
 			}
 			while (s2[j] != '\0')
-
 			{
 				concat[i] = s2[j];
 
