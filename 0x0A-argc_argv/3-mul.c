f78b4b271b3b4aa130a9082b490e5b493dca1a53
mainavict <vampmid03@gmail.com>
2023-04-13 03:45:12 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiples two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0;
+ */
+int main(int argc, char **argv)
+{
+	int x, y, prod;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	prod = (x * y);
+	printf("%d\n", prod);
+	return (0);
+}
+
