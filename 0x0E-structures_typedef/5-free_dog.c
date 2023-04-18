6f4bdcbd37fa1baa9e9823fce0483e84de995311
mainavict <vampmid03@gmail.com>
2023-04-18 02:36:49 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *  _strlen - a function that gets a length of string
+ *
+ *  @str: the string to get the length
+ *   Return: length of @str
+ */
+int _strlen(const char *str)
+{
+	int l = 0;
+
+	while (*str++)
+		l++;
+	return (length);
+}
