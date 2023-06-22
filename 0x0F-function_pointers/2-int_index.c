4a5f55d977e16a40c3febd6093c37d68d1885538
mainavict <vampmid03@gmail.com>
2023-06-21 23:20:30 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for an interger within an array
+ * @array: pointer to an array
+ * @size: size of the array
+ * @cmp: pointer to the function
+ * Return: the index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; 1 < size; i++)
+	{
+		if(cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
