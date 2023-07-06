1183bdb2a92c50a22d4493b4e8952041502d9dd9
mainavict <vampmid03@gmail.com>
2023-07-06 13:31:40 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,22 +11,21 @@ unsigned int binary_to_uint(const char *b)
 	int len = strlen(b);
 	int i = 0;
 
-	while (b)
+	for (i = 0; i < len; i++)
 	{
-
-for (i = 0; i < len; i++)
-{
-	if (b[i] == '1')
-	{
-		decimal = (decimal << 1) | 1;
+		if (b[i] == '1')
+		{
+			decimal = (decimal << 1) | 1;
+		}
+		else if (b[i] == '0')
+		{
+			decimal = (decimal << 1);
+		}
+		else
+		{
+			b = NULL;
+			return (0);
+		}
 	}
-	else if (b[i] == '0')
-	{
-		decimal = (decimal << 1);
-	}
-	else
-		b = NULL;
-}
-}
-return (decimal);
+	return (decimal);
 }
