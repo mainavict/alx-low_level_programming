d4e16d70e70a239a857d39fefdac7cae13b3de92
mainavict <vampmid03@gmail.com>
2023-07-07 02:36:11 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,15 +5,15 @@
  *  @index: the position
  *  Return: 1 if successful
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int num = 0;
+	unsigned long int num = 1;
 
 	num <<= index;
 
 	if (index > sizeof(unsigned long int) * CHAR_BIT)
 		return (-1);
-	(*n) = (*n) | num;
+	(*n) = (*n) & ~(num);
 	return (1);
 
 
