15f34eedda2b6db08c51c3c62daa87343c20ad2b
mainavict <vampmid03@gmail.com>
2023-07-11 15:01:13 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends text
+ * Description: function that appends
+ * @filename: filename
+ * @text_content: added content
+ * Return: 1 exists. -1 does not exist
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		if (write(fd, text_content, strlen(text_content)) == (ssize_t) -1)
+			return (-1);
+	close(fd);
+	return (1);
+}
